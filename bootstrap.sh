a478ef7ea0fd895a2b7a68aa4c7979d85c6f0688
Leonardo Ji <leonardoji80@gmail.com>
2018-06-19 22:55:36 -0500

    Update bootstrap.sh
    To get it work on windows.

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,6 +16,9 @@
 #
 set -e
 
+# Sometimes Windows just destroys end of lines
+apt-get install -y dos2unix
+dos2unix /vagrant/install/install-*
 # Running Install Scripts
 SCRIPTS=$(find /vagrant/install/install-* -type f)
 
